2fb9e4ec3bd8581940097bb1fbf6f1fb9993bb2d
ukicm <ukiicm@gmail.com>
2019-10-21 18:04:04 +0800

    Finished lab7.

---
+++ lab7/BSTMap.java
@@ -0,0 +1,102 @@
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K> , V> implements Map61B<K, V>{
+    public class Node {
+        private K key;
+        private V value;
+        private int size;
+        private Node left, right;
+
+        private Node(K k, V v) {
+            key = k;
+            value = v;
+            size = 1;
+        }
+    }
+
+    private Node root;
+
+    /** Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    /* Returns true if this map contains a mapping for the specified key. */
+    @Override
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+        if (x == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.size;
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, K key, V value) {
+        if (x == null) {
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    /* Returns a Set view of the keys contained in this map. */
+    Set<K> keySet();
+
+    /* Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 8. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    V remove(K key);
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 8. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    V remove(K key, V value);
+
+}
